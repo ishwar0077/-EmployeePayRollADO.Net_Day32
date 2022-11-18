d884f9ca88438bcca059140985aa06009cc686f2
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-11-18 20:47:54 +0530

    Add Employee

---
+++ EmployeeRepo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpPayroll_Day32
+{
+    public class EmployeeRepo
+    {
+
+        public static string connectionstring = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EmployeePayRoll;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionstring);
+
+
+        public bool AddEmployee(EmployeeModel model)
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    SqlCommand command = new SqlCommand("SpAddEmployeeDetails", this.connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EmployeeName", model.EmployeeName);
+                    command.Parameters.AddWithValue("@Phoneno", model.Phoneno);
+                    command.Parameters.AddWithValue("@Address", model.Address);
+                    command.Parameters.AddWithValue("@Department", model.Department);
+                    command.Parameters.AddWithValue("@Gender", model.Gender);
+                    command.Parameters.AddWithValue("@BasicPay", model.BasicPay);
+                    command.Parameters.AddWithValue("@Deduction", model.Deduction);
+                    command.Parameters.AddWithValue("@TaxablePay", model.TaxablePay);
+                    command.Parameters.AddWithValue("@Tax", model.Tax);
+                    command.Parameters.AddWithValue("@Netpay", model.NetPay);
+                    command.Parameters.AddWithValue("@StartDate", DateTime.Now);
+                    command.Parameters.AddWithValue("@City", model.City);
+                    command.Parameters.AddWithValue("@Country", model.Country);
+                    this.connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    this.connection.Close();
+                    if (result != 0)
+                    {
+                        return true;
+
+                    }
+                    return false;
+
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Payroll Program");
+            Console.WriteLine("welcome to employee payroll");
+            EmployeeModel employee = new EmployeeModel();
+            EmployeeRepo repo = new EmployeeRepo();
+            employee.EmployeeName = "sudhanshu";
+            employee.Phoneno = "789891212";
+            employee.Address = "bsp";
+            employee.Department = "hr";
+            employee.Gender = 'm';
+            employee.BasicPay = 10000;
+            employee.Deduction = 1000;
+            employee.TaxablePay = 2000;
+            employee.Tax = 111;
+            employee.NetPay = 1111;
+            employee.StartDate = DateTime.Now;////////
+            employee.City = "bsp";
+            employee.Country = "india";
+
+            repo.AddEmployee(employee);
         }
     }
 }
