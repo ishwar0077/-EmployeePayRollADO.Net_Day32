90e839af26ff554394b5ff5fafd9e1a17ce6e379
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-11-18 20:22:17 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpPayroll_Day32
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Program");
+        }
+    }
+}
