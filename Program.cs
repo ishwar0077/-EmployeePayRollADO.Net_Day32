0e2940dc4c8c2c5a31ab1bfe39c44cf904ba029f
Ishwar Rathod <ishwarkrathod.7@gmail.com>
2022-11-18 20:55:17 +0530

    Retriving tha data

--- Program.cs
+++ Program.cs
@@ -7,21 +7,23 @@
             Console.WriteLine("welcome to employee payroll");
             EmployeeModel employee = new EmployeeModel();
             EmployeeRepo repo = new EmployeeRepo();
-            employee.EmployeeName = "sudhanshu";
-            employee.Phoneno = "789891212";
-            employee.Address = "bsp";
-            employee.Department = "hr";
+            employee.EmployeeName = "Ishwar";
+            employee.Phoneno = "9140402022";
+            employee.Address = "tel";
+            employee.Department = "mh";
             employee.Gender = 'm';
             employee.BasicPay = 10000;
             employee.Deduction = 1000;
-            employee.TaxablePay = 2000;
+            employee.TaxablePay = 2000.00;
             employee.Tax = 111;
             employee.NetPay = 1111;
             employee.StartDate = DateTime.Now;////////
-            employee.City = "bsp";
+            employee.City = "tel";
             employee.Country = "india";
 
-            repo.AddEmployee(employee);
+            // repo.AddEmployee(employee);
+            repo.GetAllEmployee();
+
         }
     }
 }
